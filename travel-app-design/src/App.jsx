0c0954afcf1fcef61a280935df6aa012038c3ff5
sammadr <20212025@itla.edu.do>
2023-10-05 20:52:12 -0400

    Agregando pre-portada

--- travel-app-design/src/App.jsx
+++ travel-app-design/src/App.jsx
@@ -1,9 +1,11 @@
 import './App.css'
+import PrePortada from './componentes/prePortada/PrePortada'
 
 function App() {
   return (
     //Aquí van sus creaciones (componentes)
     <>
+    <PrePortada/>
     </>
   )
 }
--- travel-app-design/src/componentes/prePortada/PrePortada.jsx
+++ travel-app-design/src/componentes/prePortada/PrePortada.jsx
@@ -1,7 +1,25 @@
 import "./prePortadaStyle.css"
+import BgVideo from '../../medio/fondo-rio.mp4'
 
-export const PrePortada = () => {
-  return (
-    <div>PrePortada</div>
-  )
+const PrePortada = () => {
+    return (
+        <div className="preportada">
+            <video src={BgVideo} autoPlay muted loop className="video-bg" />
+              <div className="bg-overlay"></div>
+              <div className="titulo">
+                  <h1>Descubre las diferentes maravillas del mundo</h1>
+              </div>
+
+            <div className="boton-contenedor">
+                <div className="boton">Iniciar sesión</div>
+                <div className="boton">Registro</div>
+            </div>
+
+        </div>
+    )
 }
+
+export default PrePortada;
+
+
+

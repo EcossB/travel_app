9e88b667294752288b5912cd7bb7de0aa9a34940
cristalPyC <cristaltn03@gmail.com>
2023-10-17 18:18:17 -0400

    ok

--- travel-app-design/src/paginas/home/Menu.jsx
+++ travel-app-design/src/paginas/home/Menu.jsx
@@ -1,5 +1,24 @@
+import { Logo } from "../../componentes/logo/Logo";
 export const Menu = () => {
   return (
-    <div>Menu</div>
+    <header id="menu">
+      <div className="menu-logo">
+        <Logo />
+      </div>
+      <div className="links-info">
+        <ul>
+          <li>Inicio</li>
+          <li>Contacto</li>
+          <li>Trips</li>
+          <li>Nosotros</li>
+        </ul>
+      </div>
+      <div className="user-info">
+        <ul>
+          <li>Emily Smith</li>
+          <button className="logout-btn">Cerrar sesión</button>
+        </ul>
+      </div>
+    </header>
   )
 }

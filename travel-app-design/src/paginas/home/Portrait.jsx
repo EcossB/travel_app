6f9e9fb4c5ade09bd3ae82016a30dffc4c12592e
sammadr <20212025@itla.edu.do>
2023-10-20 04:39:31 -0400

    Agregando banner (Portrait)

--- travel-app-design/src/paginas/home/Portrait.jsx
+++ travel-app-design/src/paginas/home/Portrait.jsx
@@ -2,7 +2,7 @@ export const Portrait = () => {
   return (
     <section className="banner">
             <div className="banner__img">
-                <h2 className="banner__titulo">Encuentra la mejor opción para tu viaje</h2>
+                <h2 className="banner__titulo">Encuentra la mejor opción para tu viaje </h2>
             </div>
 </section>
   )

1079cfa8f7c89055d083213f67067474abe89db2
EcossB <erwingcoss21@hotmail.com>
2023-10-25 12:03:30 -0400

    Initial backend Commit.

---
+++ turisteoApi/src/main/java/com/turisteo/turisteoApi/domain/pais/ClasePrueba.java
@@ -0,0 +1,5 @@
+package com.turisteo.turisteoApi.domain.pais;
+
+//clase debe de ser borrada
+public class ClasePrueba {
+}
---
+++ turisteoApi/src/main/java/com/turisteo/turisteoApi/infra/security/ClasePrueba.java
@@ -0,0 +1,5 @@
+package com.turisteo.turisteoApi.infra.security;
+
+//clase debe de ser borrada
+public class ClasePrueba {
+}

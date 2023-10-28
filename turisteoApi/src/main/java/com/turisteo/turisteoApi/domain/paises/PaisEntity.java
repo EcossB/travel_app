17c607f13efe1addeb52bb07fb2ec620f96c5aa2
EcossB <erwingcoss21@hotmail.com>
2023-10-28 00:13:05 -0400

    relationship with other entities, construct for post and mehtod for put.

--- turisteoApi/src/main/java/com/turisteo/turisteoApi/domain/paises/PaisEntity.java
+++ turisteoApi/src/main/java/com/turisteo/turisteoApi/domain/paises/PaisEntity.java
@@ -17,12 +17,43 @@ public class PaisEntity {
     private Long pais_id;
     private String nombre;
     private String descripcion;
-    private Continentes continentes;
+
+    @Enumerated(EnumType.STRING)
+    private Continentes continente;
+
     private String bandera;
     private String imagen1;
     private String imagen2;
 
+    @OneToMany(mappedBy = "paisEntity")
+    private List<LugaresFamososEntity> lugaresFamosos;
+
+    @OneToMany(mappedBy = "paisEntity")
+    private List<GastronomiaEntity> gastronomia;
+
+    @OneToMany(mappedBy = "paisEntity")
+    private List<HotelEntity> hotel;
+
+    @OneToMany(mappedBy = "paisEntity")
+    private List<VuelosEntity> vuelos;
 
 
+    public PaisEntity(PaisDtoP paisDtoP) {
+        this.nombre = paisDtoP.nombre();
+        this.descripcion = paisDtoP.descripcion();
+        this.continente = paisDtoP.continentes();
+        this.bandera = paisDtoP.bandera();
+        this.imagen1 = paisDtoP.imagen1();
+        this.imagen2 = paisDtoP.imagen2();
+    }
+
+    public void actualizarPais(PaisDtoPut paisDtoPut){
+        this.nombre = paisDtoPut.nombre();
+        this.descripcion = paisDtoPut.descripcion();
+        this.continente = paisDtoPut.continentes();
+        this.bandera = paisDtoPut.bandera();
+        this.imagen1 = paisDtoPut.imagen1();
+        this.imagen2 = paisDtoPut.imagen2();
+    }
 }
 

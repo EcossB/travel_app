7ef82e7218cd50ff39bf79cfa11376a23b16b6cb
EcossB <erwingcoss21@hotmail.com>
2023-10-28 00:23:49 -0400

    DTO for paises

---
+++ turisteoApi/src/main/java/com/turisteo/turisteoApi/domain/paises/PaisDtoP.java
@@ -0,0 +1,22 @@
+package com.turisteo.turisteoApi.domain.paises;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+
+public record PaisDtoP(
+
+
+        @NotBlank
+        String nombre,
+        @NotBlank
+        String descripcion,
+        @NotNull
+        Continentes continentes,
+        @NotBlank
+        String bandera,
+        @NotBlank
+        String imagen1,
+        @NotBlank
+        String imagen2
+) {
+}

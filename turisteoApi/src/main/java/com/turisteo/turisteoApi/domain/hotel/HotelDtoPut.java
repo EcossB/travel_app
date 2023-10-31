e2091824fa8ca0e4b76936619842655408adbeac
Rafael Ariel Custodio Guillen <107286551+0-0Rafael@users.noreply.github.com>
2023-10-30 22:03:13 -0400

    Commit Hotel -HotelDtoP -HotelDtoPut

---
+++ turisteoApi/src/main/java/com/turisteo/turisteoApi/domain/hotel/HotelDtoPut.java
@@ -0,0 +1,24 @@
+package com.turisteo.turisteoApi.domain.hotel;
+
+import com.turisteo.turisteoApi.domain.paises.PaisEntity;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+
+public record HotelDtoPut(
+
+        @NotBlank
+        long id,
+
+        @NotBlank
+        String nombre_hotel,
+
+        @NotBlank
+        float valoracion,
+
+        @NotBlank
+        String imagen_hotel,
+
+        @NotNull
+        PaisEntity paisId
+) {
+}

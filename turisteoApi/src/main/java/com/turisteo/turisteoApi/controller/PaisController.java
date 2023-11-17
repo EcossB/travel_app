261f31060b8a36f4625b1eb1ba363a2030bfa9be
EcossB <erwingcoss21@hotmail.com>
2023-11-16 22:00:21 -0400

    Adding Security requirement, an annotation for bearer token

--- turisteoApi/src/main/java/com/turisteo/turisteoApi/controller/PaisController.java
+++ turisteoApi/src/main/java/com/turisteo/turisteoApi/controller/PaisController.java
@@ -1,6 +1,6 @@
 package com.turisteo.turisteoApi.controller;
 
-
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import com.turisteo.turisteoApi.domain.paises.*;
 import jakarta.transaction.Transactional;
 import jakarta.validation.Valid;
@@ -9,6 +9,7 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.PageableDefault;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Repository;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.util.UriComponentsBuilder;
@@ -18,6 +19,7 @@ import java.util.Optional;
 
 @RestController
 @RequestMapping("/paises")
+@SecurityRequirement(name = "bearer-key")
 public class PaisController {
 
     @Autowired
@@ -31,6 +33,7 @@ public class PaisController {
 
 
     @PostMapping
+    @PreAuthorize("hasRole('ADMIN')")
     public ResponseEntity<PaisDtoG> guardarPais(@RequestBody @Valid PaisDtoP paisDtoP , UriComponentsBuilder builder){
         System.out.println("El post fue correcto");
 

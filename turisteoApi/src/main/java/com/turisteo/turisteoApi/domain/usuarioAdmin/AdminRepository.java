4f9177684b2a4b9d54acdd97f50d147c6ff938f0
EcossB <erwingcoss21@hotmail.com>
2023-11-16 21:57:55 -0400

    Deleting files

--- turisteoApi/src/main/java/com/turisteo/turisteoApi/domain/usuarioAdmin/AdminRepository.java
+++
@@ -1,6 +0,0 @@
-package com.turisteo.turisteoApi.domain.usuarioAdmin;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface AdminRepository extends JpaRepository<Long,UsuarioAdminEntity> {
-}

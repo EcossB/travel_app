a8cf45e72295b3d485c3e36d6b635cb66b6ba782
EcossB <erwingcoss21@hotmail.com>
2023-11-17 21:40:23 -0400

    Adding a new method to search by continents

--- turisteoApi/src/main/java/com/turisteo/turisteoApi/domain/paises/PaisRepository.java
+++ turisteoApi/src/main/java/com/turisteo/turisteoApi/domain/paises/PaisRepository.java
@@ -1,8 +1,14 @@
 package com.turisteo.turisteoApi.domain.paises;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
 
 public interface PaisRepository extends JpaRepository<PaisEntity, Long> {
 
-    public PaisEntity findByNombre( String Nombre);
+    PaisEntity findByNombre( String Nombre);
+
+    List<PaisEntity> findByContinente(Continentes continente);
 }
